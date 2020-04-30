5f0c0e57e7b27a6b118eb317894220b1aabd7560
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-04-30 17:18:34 -0300

    Conversão de PX para PT em fontes e EM em espaçamentos

--- teachme-app/src/home/components/Hero.js
+++ teachme-app/src/home/components/Hero.js
@@ -5,6 +5,7 @@ function Hero() {
   return (
     <section class="hero">
         <div class="cta-text">
+            <h1>Venha para o Teach.me</h1>
             <p><strong>Seja um aluno e agende aulas em minutos!</strong><br/>Dê um upgrade na sua carreira aprendendo algo novo ou finalmente superando uma dificuldade.</p>
             <p><strong>Seja um instrutor e ganhe dinheiro compartilhando conhecimento.</strong><br/>Aqui suas habilidades valem muito.</p>
             <p>Quer aproveitar todas as vantagens que o Teach.me tem para oferecer? Faça já o seu cadastro.</p> 

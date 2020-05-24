9f706cf853fa099dfed044198fac26abc22306d9
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-05-24 17:51:18 -0300

    Ajustado os links conforme card SITEMAP

--- teachme-app/src/shared/Menu.js
+++ teachme-app/src/shared/Menu.js
@@ -19,20 +19,29 @@ function Menu(props) {
                 <nav>
                     <li><Link to="/" onClick={closeMenu}><i class="fa-fw fas fa-home"/> Home</Link></li>
                 </nav>
+
+                <nav>
+                    <li><Link to="instrutor" onClick={closeMenu}><i class="fa-fw fas fa-search"/> Buscar Instrutor</Link></li>
+                </nav>
+                
                 <nav>
                     <li><Link to="/painel" onClick={closeMenu}><i class="fa-fw fas fa-user"/> Painel</Link></li>
                     <ul>
-                        <li><Link to="/painel/meu-perfil" onClick={closeMenu}>Minha Conta</Link></li>
-                        <li><Link to="/painel/turma/minhas-turmas" onClick={closeMenu}>Minhas Turmas</Link></li>
-                        <li><Link to="#" onClick={closeMenu}>Minhas Aulas</Link></li>
+                        <li><Link to="/painel/minha-conta" onClick={closeMenu}>Minha Conta</Link></li>
+                        <li><Link to="/painel/minhas-turmas" onClick={closeMenu}>Minhas Turmas</Link></li>
+                        <li><Link to="/painel/aulas-e-propostas" onClick={closeMenu}>Aulas e Propostas</Link></li>
                     </ul>
                 </nav>
+
                 <nav>
-                    <li><Link to="instrutores" onClick={closeMenu}><i class="fa-fw fas fa-graduation-cap"/> Instrutores</Link></li>
+                    <li><Link to="/painel-instrutor" onClick={closeMenu}><i class="fa-fw fas fa-graduation-cap"/> Painel Instrutor</Link></li>
+                    <ul>
+                        <li><Link to="/painel-instrutor/horarios-e-locais" onClick={closeMenu}>Horários e Locais</Link></li>
+                        <li><Link to="/painel-instrutor/oferecimentos" onClick={closeMenu}>Meus Oferecimentos</Link></li>
+                        <li><Link to="/painel-instrutor/minhas-turmas" onClick={closeMenu}>Minhas Turmas</Link></li>
+                    </ul>
                 </nav>
-                <nav>
-                    <li><Link to="instrutores" onClick={closeMenu}><i class="fa-fw fas fa-comment"/> Chat</Link></li>
-                </nav>
+                
             </div>
 
             <div id="menu-shadow" class={props.menuState && 'visible'} onClick={closeMenu}>&nbsp;</div>

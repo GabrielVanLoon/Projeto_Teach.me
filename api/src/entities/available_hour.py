ea6607824afb3172d1207653451f0fb584b0dc2d
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-07-11 19:27:13 -0300

    ADD final entities

---
+++ api/src/entities/available_hour.py
@@ -0,0 +1,6 @@
+
+class AvailableHour:
+    def init(self, instructor = None, weekday = None, hour = None ):
+        self.instructor = instructor 
+        self.weekday = weekday 
+        self.hour = hour
---
+++ api/src/entities/lesson.py
@@ -0,0 +1,12 @@
+
+class Lesson:
+    def __init__(self, proposal = None, lesson_number = None, instructor = None, local = None, final_price = None, status = None, start = None, end = None, instructor_rate = None):
+        self.proposta = proposal 
+        self.lesson_number = lesson_number 
+        self.instructor = instructor 
+        self.local = local 
+        self.final_price = final_price 
+        self.status = status 
+        self.start = start 
+        self.end = end 
+        self.instructor_rate = instructor_rate

f93208f53568912479c26a3af761ecab72da2260
AlbertWolf99 <alberto.cneves@gmail.com>
2020-07-13 15:42:15 -0300

    Normalized place's address

--- api/src/entities/place.py
+++ api/src/entities/place.py
@@ -1,9 +1,11 @@
 class Place:
-    def __init__(self, instructor=None, placename=None, capacity=None, address=None, complement=None, city=None, federal_state=None):
+    def __init__(self, instructor=None, placename=None, capacity=None, street=None, number=None, neighborhood=None, complement=None, city=None, federal_state=None):
         self.instructor = instructor
         self.placename = placename
         self.capacity = capacity
-        self.address = address
+        self.street = street
+        self.number = number
+        self.neighborhood = neighborhood
         self.complement = complement
         self.city = city
         self.federal_state = federal_state

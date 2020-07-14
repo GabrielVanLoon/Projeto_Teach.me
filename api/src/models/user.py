30ddc49b698fce6eb207b80b162befbb29830287
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-07-13 21:00:10 -0300

    ADD validation functions

--- api/src/models/user.py
+++ api/src/models/user.py
@@ -1,31 +1,33 @@
+from django.http import QueryDict
 import re 
 
+from src.libs.validations import *
 from src.entities.user import User
 from src.dao.user import UserDAO
 
 class UserModel:
 
-    def register(self, args = None):
+    def register(self, args:QueryDict = None):
         user = None
 
         # 1º Pegando os parâmetros de interesse
         try: 
             user = User(args.get('username', '').strip(), args.get('email', '').strip(), args.get('password', '').strip(), 
-                args.get('name', '').strip(), args.get('last_name', '').strip(), None, False)
+                            args.get('name', '').strip(), args.get('last_name', '').strip(), None, False)
         except Exception as e:
             print('[userModel.register]', str(e))
             raise Exception('invalid arguments.')
 
-        # 2º Validando inputs
-        if (user.username == '') or (len(user.username) < 2):
+        # 2º Validando inputs        
+        if (user.username == '') or (len(user.username) < 2) or (not is_username(user.username)):
             raise Exception('invalid username field.')
-        if not re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', user.email):
+        if not is_valid_mail(user.email):
             raise Exception('invalid email field.')
         if (user.password == '') or (len(user.password) < 8):
             raise Exception('invalid password field.')
-        if (user.name == '') or (user.last_name == ''):
+        if (not is_alphabetic(user.name, with_spaces=True)) or (not is_alphabetic(user.last_name, with_spaces=True)):
             raise Exception('invalid name and last name fields')
-
+        
         # 3º Realizando o registro
         try:
             UserDAO().insert(user)

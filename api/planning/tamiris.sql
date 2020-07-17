73379e1f262c7e616be626e27c0237c1807d055c
TamirisTinelli <tamiris.tinelli@usp.br>
2020-07-17 14:37:16 -0300

    Queries finalizadas

--- api/planning/tamiris.sql
+++ api/planning/tamiris.sql
@@ -1,7 +1,7 @@
 - selecionar a média de preço base por disciplina
     [Fácil - GroupBy - Tamiris]
 
-SELECT AVG(O.PRECO_BASE), O.DISCIPLINA
+SELECT O.DISCIPLINA, ROUND(AVG(O.PRECO_BASE), 2) AS MEDIA
     FROM OFERECIMENTO O
     GROUP BY O.DISCIPLINA;
 -----------------------------------------------------------------------------------
@@ -20,32 +20,71 @@ SELECT AL.DATA_INICIO, PP.TURMA, PP.DISCIPLINA, AL.INSTRUTOR --, PA.ALUNO
 	FROM AULA AL
     JOIN PROPOSTA PP ON (AL.PROPOSTA = PP.ID) -- para descobrir a turma
     JOIN PARTICIPANTE PA ON (PA.TURMA = PP.TURMA) -- para descobrir o usuario
-    WHERE UPPER(AL.STATUS) = 'AGENDADA' AND UPPER(PA.ALUNO) = 'BOB'
+    WHERE UPPER(AL.STATUS) = 'AGENDADA' AND PA.ALUNO = 'bob'
 	AND AL.DATA_INICIO > CURRENT_TIMESTAMP
 	ORDER BY AL.DATA_INICIO;
+
+--otimizado:
+SELECT AL.DATA_INICIO, AC.TURMA, AL.INSTRUTOR, AL.PROPOSTA, AL.NUMERO
+	FROM AULA AL
+    JOIN ACEITA AC ON (AC.PROPOSTA = AL.PROPOSTA) -- para descobrir o usuario
+    WHERE UPPER(AL.STATUS) = 'AGENDADA' AND AC.ALUNO = 'bob'
+		AND AL.DATA_INICIO > CURRENT_TIMESTAMP
+	ORDER BY AL.DATA_INICIO;    
 -----------------------------------------------------------------------------------
-- Pegar média de nota de instrutor para cada matéria que ensina (se der aula de mais de uma matéria)
+- Pegar média de nota de um instrutor para cada matéria que ensina (se der aula de mais de uma matéria)
     [Fácil ou Médio? - Junções e group_by - Tamiris]
 
-SELECT AVG(AP.NOTA), PP.DISCIPLINA
+SELECT PP.DISCIPLINA, ROUND(AVG(AP.NOTA), 2) AS MEDIA_AVALIACAO
     FROM AVALIACAO_PARTICIPANTE AP
     JOIN PROPOSTA PP ON (PP.ID = AP.PROPOSTA)
-	WHERE UPPER(PP.INSTRUTOR) = 'ANA'
+	WHERE PP.INSTRUTOR = 'ana'
     GROUP BY PP.DISCIPLINA;
 -----------------------------------------------------------------------------------
 - Listar as turmas que não obtiveram NENHUMA avaliação abaixo de 3 em nenhuma de suas aulas.
     [Média - Tamiris]
+--1A FORMA
+SELECT DISTINCT PP.TURMA
+    FROM PROPOSTA PP
+	JOIN AULA AL ON (AL.PROPOSTA = PP.ID)
+	WHERE PP.STATUS IN ('APROVADA', 'FINALIZADA')
+		AND (AL.STATUS = 'FINALIZADA')
+	EXCEPT 
+	SELECT PP.TURMA
+		FROM PROPOSTA PP
+		JOIN AULA AL ON (AL.PROPOSTA = PP.ID)
+		WHERE PP.STATUS IN ('APROVADA', 'FINALIZADA')
+			AND (AL.NOTA_INSTRUTOR < 3)
 
-SELECT T.NOME_TURMA
-    FROM TURMA T
-    JOIN PROPOSTA P ON (T.NOME = P.TURMA)
-    JOIN AULA A ON (P.ID = A.PROPOSTA)
-    WHERE MIN (A.NOTA_INSTRUTOR > 3.0);
+-- 2A FORMA - MAIS EFICIENTE
+SELECT PP.TURMA
+    FROM PROPOSTA PP
+	JOIN AULA AL ON (AL.PROPOSTA = PP.ID)
+	WHERE PP.STATUS IN ('APROVADA', 'FINALIZADA')
+		AND AL.NOTA_INSTRUTOR IS NOT NULL
+	GROUP BY PP.TURMA
+	HAVING MIN (AL.NOTA_INSTRUTOR) > 3
 -----------------------------------------------------------------------------------
-- Instrutores que deram a maior quantidade de aulas em cada estado no último mês. Em casos de empate exibir todos.
-    [Médio - Tamiris]
+- Instrutores que deram a maior quantidade de aulas em cada estado no último mês.
+    [Médio - Dificil- Tamiris]
+--select interno, exibe a quantidade de aulas por instrutor em cada estado
+SELECT AL.INSTRUTOR, L.UF, COUNT (AL.NUMERO)
+	FROM LOCAL L
+	JOIN AULA AL ON (L.INSTRUTOR = AL.INSTRUTOR AND L.NOME = AL.LOCAL)
+	WHERE (AL.STATUS = 'FINALIZADA')
+		AND AL.DATA_INICIO >= ('now'::timestamp - '1 month'::interval)
+	GROUP BY AL.INSTRUTOR, L.UF
+	ORDER BY L.UF, COUNT (AL.NUMERO) DESC
 
-SELECT I.NOME_USUARIO, COUNT (AL.NUMERO)
-    FROM INSTRUTOR I
-    JOIN AULA AL ON (AL.INSTRUTOR = I.NOME_USUARIO)
-    WHERE (AL.STATUS = 'FINALIZADA');
+--query completa, seleciona apenas as maiores quantidades de aulas
+SELECT TODOS.UF, (array_agg(TODOS.INSTRUTOR))[1], MAX (TODOS.N_AULAS)	
+	FROM (
+		SELECT AL.INSTRUTOR, L.UF, COUNT (AL.NUMERO) AS N_AULAS
+			FROM LOCAL L
+			JOIN AULA AL ON (L.INSTRUTOR = AL.INSTRUTOR AND L.NOME = AL.LOCAL)
+			WHERE (AL.STATUS = 'FINALIZADA')
+				AND AL.DATA_INICIO >= ('now'::timestamp - '1 month'::interval)
+			GROUP BY AL.INSTRUTOR, L.UF
+			ORDER BY L.UF, COUNT (AL.NUMERO) DESC )
+	AS TODOS
+	GROUP BY TODOS.UF

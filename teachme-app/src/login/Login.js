8cc5291f9dcdc2494040da994812599a102ada14
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-07-18 17:07:44 -0300

    ADD Login Flow

--- teachme-app/src/login/Login.js
+++ teachme-app/src/login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import API from "../utils/API";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import './Login.css';
 
 import { Router, Link, redirectTo } from "@reach/router"
@@ -10,26 +10,49 @@ function Login() {
   // Os estados do componente se referem aos campos
   const [username, setUsername] = useState(""); 
   const [senha, setSenha] = useState(""); 
+  const [loginIncorreto, setLoginIncorreto] = useState(false); 
 
-  const handleSubmit = (evt) => {
-      evt.preventDefault();
-      console.log(`Submitting Name ${username} Senha ${senha}`)
+  const usernameInput = useRef(null);
+  const senhaInput    = useRef(null);
 
-      let data = `username=${username}&password=${senha}`
- 
-      API.post('login', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }} )
-      .then(response => {
-        console.log('SUCESSO')
-        console.log(response)
-        console.log(`Olá: ${response.data.username}`)
-      })
-      .catch(error => {
-          console.log('ERRO')
-          console.log(error.response)
-      });
+
+  function validarCampos(){
+    usernameInput.current.style.borderColor = null
+
+    if(!username.match(/^[a-zA-Z0-9-_]{1,}$/)){
+      alert("Username com caractéres inválidos.")
+      usernameInput.current.focus()
+      return false
+    } 
+
+    return true;
   }
 
-  
+  function enviarDados(){
+    let data = `username=${username}&password=${senha}`
+
+    API.post('user', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }} )
+    .then(response => {
+      localStorage.setItem('username', response.data.user.username)
+      window.location.reload(false);
+    })
+    .catch(error => {
+      usernameInput.current.focus()
+      setLoginIncorreto(true)
+    });
+  }
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault()
+     
+    // Validação 
+    if(!validarCampos())
+        return;
+
+    // Enviando os dados
+    enviarDados()
+} 
+
   return (
     <main id="page-login" class="body-card">
         <h1 class="text-center">Bem vindo ao <span id="text-name">Teach.me</span></h1>
@@ -39,19 +62,23 @@ function Login() {
           <div>
             <div class="form-group">
               <label for="username">Nome de usuário</label>
-              <input type="text" name="username" 
-                value={username} onChange={e => setUsername(e.target.value)} />
+              <input type="text" name="username" minLength={2} maxLength={30} required
+                ref={usernameInput} value={username} onChange={e => setUsername(e.target.value)} />
             </div>
 
             <div class="form-group">
               <label for='password'>Senha</label>
-              <input type="password" name="password"
-                value={senha} onChange={e => setSenha(e.target.value)} />
+              <input type="password" name="password" minLength={8} required
+                ref={senhaInput} value={senha} onChange={e => setSenha(e.target.value)} />
             </div>
           </div>
           <p class="right-btn">
+            { loginIncorreto && 
+              <span for="username" style={{'color': 'red', 'margin-right': '10px'}}>Usuário ou Senha inválidos.</span>
+            }
             <button type="submit" class="btn" name="btn-login">Entrar</button>
           </p>
+         
         </form>
         
         <p><small>Ainda não possui uma conta? Clique aqui e <Link to="/criar-conta">cadastre-se.</Link></small></p>
